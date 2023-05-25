c5995c9111b4f06cc043a4ca01ed660392fd3226
Daniel <dannyjaygil@gmail.com>
2023-05-25 13:39:06 +0300

    Make hoverColor function that paints grid black

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ let gridBox;
 
 for (let i = 1; i <= 16 ** 2; i++) {
   gridBox = document.createElement("div");
+  gridBox.classList.add("box");
   container.appendChild(gridBox);
+
+  //   gridBox.addEventListener("mouseover", function () {
+  //     gridBox.classList.toggle("color");
+  //   });
+
+  hoverColor();
 }
 
-// gridBox.addEventListener("mouseover", function () {
-//   gridBox.classList.toggle("color");
-// });
+function hoverColor() {
+  let items = document.querySelectorAll(".box");
+  items.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = "black";
+    });
+  });
+}
