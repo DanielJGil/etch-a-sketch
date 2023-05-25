6db7c0f032f51b755afc5bf9724c7b86d8b89b23
Daniel <dannyjaygil@gmail.com>
2023-05-25 21:49:15 +0300

    Add SLIDER function to change pixel size

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const container = document.querySelector(".container");
+const slider = document.getElementById("slider");
+let squares = slider.value;
 
-for (let i = 1; i <= 16 ** 2; i++) {
+for (let i = 1; i <= squares ** 2; i++) {
   let gridBox = document.createElement("div");
   gridBox.classList.add("box");
   container.appendChild(gridBox);
 
   hoverColor();
 }
+
+slider.addEventListener("change", () => {
+  let squares = slider.value;
+
+  container.innerHTML = "";
+
+  container.style.gridTemplateRows = `repeat(${squares}, 1fr )`;
+  container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+
+  for (let i = 1; i <= squares ** 2; i++) {
+    let gridBox = document.createElement("div");
+    gridBox.classList.add("box");
+    container.appendChild(gridBox);
+
+    hoverColor();
+  }
+});
+
+// slider.value = 16;
+// console.log(slider);
 
 function hoverColor() {
   let items = document.querySelectorAll(".box");
